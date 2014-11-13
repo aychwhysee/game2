2388d8da7cc4889dc31b18559df820a0ca71a164
Harry Cho <hacho@vassar.edu>
2014-11-12 20:24:01 -0500

    Brainstorming. I suck at thinking up games
    plz help (also don't want it to get complicated..)

--- game2/src/game2/TestyGame.java
+++ game2/src/game2/TestyGame.java
@@ -1,6 +1,40 @@
 package game2;
 
 public class TestyGame {
+    
+    /*
+    Game ideas: idea 1
+    
+    *** PERSISTENT PLAYER ATTRIBUTES
+    - You gain levels based on how much time you spend in the game
+        - As you gain levels, your "attack" (ATT) stat increases, making it easier
+          to kill mobs. Enemy HP decreases by player ATT every time
+          player hits enemy.
+    
+    *** INDEPENDENT MOBS
+    - One massive map.
+    - There are only a set number of mobs in the game, however, with each
+      mob having a different amount of HP (color coded?). Your objective
+      is to clear the game as quickly as possible. Of course, since you passively
+      gain levels and ATT, the game will get easier to clear as more time passes.
+        - You are allowed to just AFK and gain levels and come back later and
+          clear the game, but the timer is always going.
+        - So the point would be to see if you can clear the game quickly with
+          as low of a player level as possible (but the level isn't what counts - it's the time)
+    - Mobs will just be roaming around by themselves doing whatever. If they
+      get hit, they will move the opposite direction of you (or run away from you).
+    - Each mob you kill will give you a certain amount of money (based on the mob's HP)
+    
+    - You can't die. You can't lose the game. Just for simplicity. Your validation
+      of success depends on how fast you can clock the game.
+    
+    *** MODES
+    - So we have the main mode, as described above
+    - You can open up a store menu (other mode!). At the store, you can buy (with your money) items
+      that will increase your ATT stat, or increase your movement speed in the game.
+        - These items are stackable but will increase in price as you buy more. Be wise in your decisions.
+            - (Diminished rewards, son)
+    */
 
     public static void main(String[] args) {
         System.out.println("hello world!");
