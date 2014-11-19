bc1ad9548b7d7a0657307204cad7f42d57fcfd89
Harry Cho <hacho@vassar.edu>
2014-11-19 15:06:11 -0500

    stuck on mob/player interaction; moving on

---
+++ game2/src/game2/ShopItem.java
@@ -0,0 +1,8 @@
+package game2;
+
+public class ShopItem {
+    
+    // do i want to make my own list interface for items too? i don't think
+    // it'll be necessary, though I'm going to have different items. hmm.
+    
+}
