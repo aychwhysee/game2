c7b8cb60083e9449e0288f5c5bd645cf051f9b99
Harry Cho <hacho@vassar.edu>
2014-11-24 13:55:57 -0500

    Thnking... :(

--- game2/src/game2/ShopItem.java
+++ game2/src/game2/ShopItem.java
@@ -4,5 +4,24 @@ public class ShopItem {
     
     // do i want to make my own list interface for items too? i don't think
     // it'll be necessary, though I'm going to have different items. hmm.
+    // ^ maybe needed to display it in the world? display as list..hmm
+    
+    
+    // every time an item is "bought," need to decrease player money attribute
+    // and increase either speed or ATT. Where would I have the chcker be for this?
+    // Check which item the cursor (ShopCurser.java) is pointing at when
+    // player hits a key (Z) and then change stats of player..hmm..so. Constructor..
+    
+    public String item_name;
+    public int cost;
+    public int att_boost;
+    public int spd_boost;
+    
+    public ShopItem(String item_name, int cost, int att_boost, int spd_boost) {
+        this.item_name = item_name;
+        this.cost = cost;
+        this.att_boost = att_boost;
+        this.spd_boost = spd_boost;
+    }
     
 }
