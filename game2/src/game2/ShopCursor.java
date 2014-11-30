2c40773aa07c9c1c1bb0a6aa407c4e9e8d9cc1f3
Harry Cho <hacho@vassar.edu>
2014-11-30 14:11:38 -0500

    Yep

--- game2/src/game2/ShopCursor.java
+++ game2/src/game2/ShopCursor.java
@@ -1,16 +1,16 @@
-package game2;
-
-public class ShopCursor {
-    
-    // Just be a blue filled circle. down and up moves it back and forth
-    // between selections
-    
-    // have a checker to see which ShopItem it is lined up with so that when
-    // player selects to purchase that item it'll actually purchase it (if
-    // player has enough money)
-    
-    // maybe instead of having one item getting more and more expensive as you
-    // buy it (diminished rewards), I could just have a long list of items you 
-    // can buy, but each succeeding item is more pricey than the other.
-    
-}
+//package game2;
+//
+//public class ShopCursor {
+//    
+//    // Just be a blue filled circle. down and up moves it back and forth
+//    // between selections
+//    
+//    // have a checker to see which ShopItem it is lined up with so that when
+//    // player selects to purchase that item it'll actually purchase it (if
+//    // player has enough money)
+//    
+//    // maybe instead of having one item getting more and more expensive as you
+//    // buy it (diminished rewards), I could just have a long list of items you 
+//    // can buy, but each succeeding item is more pricey than the other.
+//    
+//}
