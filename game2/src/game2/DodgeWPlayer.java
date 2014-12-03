bafc3853dc359f7da861301b0af374b16ea668df
Harry Cho <hacho@vassar.edu>
2014-12-03 13:47:01 -0500

    2nd mode player setup
    hopefully doing the right thing? idk

--- game2/src/game2/DodgeWPlayer.java
+++ game2/src/game2/DodgeWPlayer.java
@@ -11,5 +11,34 @@ import javalib.worldcanvas.*;
 import javalib.worldimages.*;
 
 public class DodgeWPlayer {
+    
+//    public AttackWPlayer awp;
+    public Posn posn;
+    
+    public int b_width;
+    public int b_height;
+    
+    public int width = 15;
+    public int height = 20;
+    
+//    public int movementSpeed = awp.movementSpeed;
+//    public int attackStat = awp.attackStat;
+//    public int money = awp.money;
+    
+    public int movementSpeed;
+    public int attackStat;
+    public int money;
+    
+    public IColor color = new Blue();
 
+    public DodgeWPlayer(AttackWPlayer awp) { // make sure 2nd world player is same?
+        this.posn = awp.posn;
+        this.b_width = awp.b_width;
+        this.b_height = awp.b_height;
+        this.movementSpeed = awp.movementSpeed;
+        this.attackStat = awp.attackStat;
+        this.money = awp.money;
+    }
+    
+    
 }
