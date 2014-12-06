e9be177dcd924db224f2b265811aee8549dbb88b
Harry Cho <hacho@vassar.edu>
2014-12-06 16:07:20 -0500

    cleaning, ready

--- game2/src/game2/TestyNeverest.java
+++ game2/src/game2/TestyNeverest.java
@@ -11,43 +11,8 @@ import javalib.worldimages.*;
 
 public class TestyNeverest {
 
-    /*
-     Game ideas: idea 1: NEVEREST (arbitrary name)
-    
-     *** PERSISTENT PLAYER ATTRIBUTES
-     - You gain levels based on how much time you spend in the game
-     - As you gain levels, your "attack" (ATT) stat increases, making it easier
-     to kill mobs. Enemy HP decreases by player ATT every time
-     player hits enemy.
-    
-     *** INDEPENDENT MOBS
-     - One massive map.
-     - There are only a set number of mobs in the game, however, with each
-     mob having a different amount of HP (color coded?). Your objective
-     is to clear the game as quickly as possible. Of course, since you passively
-     gain levels and ATT, the game will get easier to clear as more time passes.
-        - Rethinking - do we even need more than one mob? Why not just one giant mob
-          with a ton of HP that we need to strategically maneuver around to kill
-          as quickly as possible????
-     - The point would be to see if you can clear the game quickly with
-     as low of a player level as possible.
-     - Mobs will just be roaming around by themselves doing whatever. If they
-     get hit, they will move the opposite direction of you (or run away from you).
-     
-    
-     - You can't die in this mode. You can't lose the game. Just for simplicity. Your validation
-     of success depends on how fast you can clock the game.
-    
-     *** MODES
-     - So we have the main mode, as described above
-     - Second mode is the reverse! Player will die (game will end) if player gets
-       hit by a mob. Player's job is to avoid/dodge all mobs for a set amount of time.
-       then it switches to first mode
-     */
     public static void main(String[] args) {
         System.out.println("hello world!");
-        //....I need ideas!
-        
         /*
         Things to test/check
         - Player movement speed works
