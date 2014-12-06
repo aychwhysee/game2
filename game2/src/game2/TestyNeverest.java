5ac11bc0425fbfda319d7b26df12f522ccfaf2ca
Harry Cho <hacho@vassar.edu>
2014-12-06 16:27:18 -0500

    aight
    time to test

--- game2/src/game2/TestyNeverest.java
+++ game2/src/game2/TestyNeverest.java
@@ -11,21 +11,37 @@ import javalib.worldimages.*;
 
 public class TestyNeverest {
 
+    public AttackWMob mob;
+    public AttackWPlayer player;
+    public int score;
+    public int timer;
+    public boolean gameOver;
+    public Posn posn;
+
+    public static Random rand = new Random();
+
+    public static int randomInt(int min, int max) {
+        return rand.nextInt((max - min) + 1) + min;
+    }
+
     public static void main(String[] args) {
         System.out.println("hello world!");
         /*
-        Things to test/check
-        - Player movement speed works
-        - Player attack stat deals right amount of damage to mob
-        - Mobs move "randomly" (at least 3 ticks in one direction, then switch)
-        - Mobs run away when hit by player
+         Things to test/check
+         - Player movement speed works
+         - Mob runs away from player
+         - Mob HP drops by player ATT stat
+         - 
         
-        - Mode changes (to diff world) every x seconds/ticks
-            - Need method to change between each world? Using..ticks? -shrug-
-        - Player dies/game ends if they get hit in 2nd mode
-        - mode changes back to regular world after set amount of time
+         - Mode changes (to diff world) every x seconds/ticks
+         - and vice versa
         
-        */
+         - Player dies/game ends if they get hit in 2nd mode
+        - mob chases player
+        
+
+        
+         */
     }
 
 }
