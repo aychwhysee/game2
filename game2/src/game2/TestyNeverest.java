dcdbe092528ad74ca28d05a4fcc94d2dbe039763
Harry Cho <hacho@vassar.edu>
2014-12-06 20:59:12 -0500

    testMobMove

--- game2/src/game2/TestyNeverest.java
+++ game2/src/game2/TestyNeverest.java
@@ -66,6 +66,7 @@ public class TestyNeverest {
         Player np2 = p.move("right");
         Player np3 = p.move("up");
         Player np4 = p.move("down");
+        Player np5 = p.move("z");
         return t.checkExpect(p.posn.x - p.movementSpeed,
                 np1.posn.x, "test move - left" + "\n")
                 && t.checkExpect(p.posn.x + p.movementSpeed,
@@ -73,7 +74,28 @@ public class TestyNeverest {
                 && t.checkExpect(p.posn.y - p.movementSpeed,
                         np3.posn.y, "test move - up" + "\n")
                 && t.checkExpect(p.posn.y + p.movementSpeed,
-                        np4.posn.y, "test move - down" + "\n");
+                        np4.posn.y, "test move - down" + "\n")
+                && t.checkExpect(p.posn,
+                        np5.posn, "test move - false case" + "\n");
+    }
+
+    public boolean testMobMove(Tester t) {
+        Mob m = randomMob();
+        Mob m1 = m.move(1);
+        Mob m2 = m.move(2);
+        Mob m3 = m.move(3);
+        Mob m4 = m.move(4);
+        Mob m5 = m.move(5);
+        return t.checkExpect(m.posn.x - m.speed,
+                m1.posn.x, "test move - left" + "\n")
+                && t.checkExpect(m.posn.x + m.speed,
+                        m2.posn.x, "test move - right" + "\n")
+                && t.checkExpect(m.posn.y - m.speed,
+                        m3.posn.y, "test move - up" + "\n")
+                && t.checkExpect(m.posn.y + m.speed,
+                        m4.posn.y, "test move - down" + "\n")
+                && t.checkExpect(m.posn,
+                        m5.posn, "test move - false case" + "\n");
     }
 
     public static void main(String[] args) {
